4e115d792bc295a24fe0105bd3ffb5fda46c9ece
roll <eskarev@gmail.com>
2017-12-08 21:43:57 +0300

    Fixed faketty

--- lib/faketty.rb
+++ lib/faketty.rb
@@ -4,5 +4,5 @@ require 'shellwords'
 # Module API
 
 def apply_faketty(code, faketty: false)
-  return faketty ? "script -qefc #{Shellwords.escape(code)}" : code
+  return faketty ? "script -qefc #{Shellwords.escape(code)} /dev/null" : code
 end
